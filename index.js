33ba1fbda555174d4ed98cb65ad9ceade036dce5
PrakharSharma888 <prakharentp2002@gmail.com>
2024-07-11 15:47:59 +0530

    Init

---
+++ index.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+mongoose.connect(
+  "mongodb+srv://prakhar:IXUr1S2b9FTEXdZc@cluster0.mmehvfp.mongodb.net/learn"
+);
+
+const app = express();
+
+const contactSchema = {
+    email: String,
+    query: String,
+ }; 
+
+const Contract = mongoose.model("Contact", contactSchema);
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static(__dirname + '/public'));
+
+app.post('/contract', function(req, res){
+    console.log("Post request received");
+})
+
+app.listen(3000,
+    function () {
+        console.log("App is running on Port 3000");
+    });
